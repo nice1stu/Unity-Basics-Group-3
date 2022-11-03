00aa2217c0e938223dbd7c0496d085ac029d66dc
KaleSmug <karl.sandegren@gmail.com>
2022-11-03 10:18:27 +0100

    Car enterable

--- SmallTheftAuto/Assets/Test3Assets/Driver.cs
+++ SmallTheftAuto/Assets/Test3Assets/Driver.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float enterCarRange = 2;
+    private Vehicle vehicle;
+
+
+    private void Update()
     {
-        
+        EnterExitCar();
     }
 
-    // Update is called once per frame
-    void Update()
+    void EnterExitCar()
     {
-        
+        if (EnterCarInput)
+        {
+            if (IsWithinCarDistance)
+            {
+                EnterCar();
+            }
+        }
+    }
+
+
+
+    private bool EnterCarInput => Input.GetButtonDown("InteractInput");
+
+    Transform GetClosestCar()
+    {
+        Transform closestCar = null;
+        Vehicle[] vehicles = FindObjectsOfType<Vehicle>();
+        float minDist = Mathf.Infinity;
+        foreach (Vehicle vehicle in vehicles)
+        {
+            Transform t = vehicle.transform;
+            float dist = Vector3.Distance(t.position, transform.position);
+            if (dist < minDist)
+            {
+                closestCar = t;
+                minDist = dist;
+            }
+        }
+
+        if (closestCar == null)
+            return null;
+
+        vehicle = closestCar.GetComponent<Vehicle>();
+        return closestCar;
+
+    }
+
+    private bool IsWithinCarDistance => Vector3.Distance(transform.position, GetClosestCar().position) <= enterCarRange;
+
+    void EnterCar()
+    {
+        transform.position = GetClosestCar().transform.position;
+        transform.SetParent(GetClosestCar());
+        gameObject.SetActive(false);
+        vehicle.driving = true;
     }
 }
