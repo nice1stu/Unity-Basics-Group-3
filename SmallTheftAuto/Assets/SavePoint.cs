930bd9ee4f8e7c1ec369697555589939061f7b6e
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-08 12:54:09 +0100

    Update SavePoint.cs

--- SmallTheftAuto/Assets/SavePoint.cs
+++ SmallTheftAuto/Assets/SavePoint.cs
@@ -6,9 +6,23 @@ using UnityEngine;
 public class SavePoint : MonoBehaviour
 {
     public GameObject player;
+    public int moneyCollected;
+    public int playerHealth;
     // Update is called once per frame
     void Update()
     {
-        //OnCollisionEnter
+        void OnCollisionEnter()
+        {
+            Vector3 playerPosition = player.transform.position;
+            PlayerPrefs.SetFloat("playerPositionX", playerPosition.x);
+            PlayerPrefs.SetFloat("playerPositionY", playerPosition.y);
+            PlayerPrefs.SetFloat("playerPositionZ", playerPosition.z);
+            PlayerPrefs.SetInt("moneyCollected", moneyCollected);
+            PlayerPrefs.SetInt("playerHealth", playerHealth);
+            //PlayerPrefs.SetString("dollars", dollars.text);
+            PlayerPrefs.Save();
+            Debug.Log("playerPosition" + playerPosition + "PlayerHealth" + playerHealth + "moneyCollected" +
+                      moneyCollected);
+        }
     }
 }
