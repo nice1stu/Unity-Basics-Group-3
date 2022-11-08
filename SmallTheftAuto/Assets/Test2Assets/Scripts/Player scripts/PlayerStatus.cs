6d284c58be3d5b727fa81ecd0dcde092dfc21067
M1crob1ology <lucabelluso123@gmail.com>
2022-11-08 15:12:02 +0100

    Added IDamageable interface

--- SmallTheftAuto/Assets/Test2Assets/Scripts/Player scripts/PlayerStatus.cs
+++ SmallTheftAuto/Assets/Test2Assets/Scripts/Player scripts/PlayerStatus.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatus : MonoBehaviour
+public class PlayerStatus : MonoBehaviour, IDamageable
 {
     public NumericValue hp;
     
