beff48144996750ab964754a1bef62676b885f32
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-08 06:06:29 +0100

    create button methods on click

---
+++ SmallTheftAuto/Assets/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game...");
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restart Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exit Game ... Thanks for playing");
+    }
+}
