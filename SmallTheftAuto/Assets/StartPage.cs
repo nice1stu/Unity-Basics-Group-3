64abf83ba921d0555c5e18a1a02cb2768a8866d9
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-08 06:57:50 +0100

    Start Page Main Menu

---
+++ SmallTheftAuto/Assets/StartPage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPage : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit Game... Thanks for playing");
+    }
+}
