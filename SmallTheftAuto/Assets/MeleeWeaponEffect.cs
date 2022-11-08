eac66c0a17dae8f4106d0e0b34da37088c60049b
M1crob1ology <lucabelluso123@gmail.com>
2022-11-08 13:37:57 +0100

    Updated player prefab

--- SmallTheftAuto/Assets/MeleeWeaponEffect.cs
+++ SmallTheftAuto/Assets/MeleeWeaponEffect.cs
@@ -1,17 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SemiAutoWeaponTrigger : MonoBehaviour
+public class MeleeWeaponEffect : MonoBehaviour
 {
     public float cooldownDuration = 0.3f;
+    public ParticleSystem hitParticle;
     private bool onCooldown;
-    private IWeaponEffect weaponEffect;
-
+    private Collider hitCol;
+    
     // Start is called before the first frame update
     void Start()
     {
-        weaponEffect = GetComponent<IWeaponEffect>();
+        hitCol = GetComponent<Collider>();
+        hitCol.enabled = false;
     }
 
     // Update is called once per frame
@@ -19,16 +22,23 @@ public class SemiAutoWeaponTrigger : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && !onCooldown)
         {
-            weaponEffect.SpawnEffect();
-            StartCoroutine(StartCooldown());
-        }
-
-        if (Input.GetButtonDown("ReloadInput"))
-        {
-            weaponEffect.ReloadEffect();
+            HitEffect();
         }
     }
 
+    void HitEffect()
+    {
+        hitCol.enabled = true;
+        hitParticle.Play();
+        StartCoroutine(StartCooldown());
+        hitCol.enabled = false;
+    }
+    
+    private void OnTriggerEnter(Collider col)
+    {
+        
+    }
+    
     public IEnumerator StartCooldown()
     {
         onCooldown = true;
